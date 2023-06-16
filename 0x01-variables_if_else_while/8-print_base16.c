ae88a851ba5d4b4d7c4d5a36e7dfd0551b63a438
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-16 16:48:02 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+		{
+			for (c = 'a'; c <= 'f'; c++)
+			{
+				putchar(c);
+				if (c == 'f')
+				{
+					putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
