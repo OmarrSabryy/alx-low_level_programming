5997e445a19f1342b051852180cef97eece844de
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-20 19:11:24 -0700

    xtable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			m = i * j;
-			if (i > 1 &&  m > 0 && m >= ((i - 1) * 10))
+			if (i > 1 &&  m > 0 && m >= 10)
 			{
 				_putchar((i - 1) + '0');
 				_putchar((m % 10) + '0');
