069a9c1320254f7c1e35c38408f9b71844c4295d
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-20 18:41:13 -0700

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			m = i*j;
+			if (m > (i-1) * 10)
+			{
+				_putchar((i - 1) + '0');
+				_putchar(m % 10);
+			}
+			else
+			{
+				_putchar(m +'0');
+			}
+			if (!(j = 9))
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
