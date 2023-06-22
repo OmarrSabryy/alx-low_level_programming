51e243a933592f7f8d47f44fa9bcada08da5a20c
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-22 16:07:37 -0700

    fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 1;
 
-	while (i < 100)
+	while (i <= 100)
 	{
 		if ((i % 15) == 0)
 		{
@@ -26,7 +26,7 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		if (!(i == 99))
+		if (!(i == 100))
 		{
 			printf(" ");
 		}
