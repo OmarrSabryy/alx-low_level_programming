727ebe422a06cb7c3dad4fa48b7002eee3b41261
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-22 09:45:45 -0700

    jdj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,19 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < 15; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (!(i > 9))
+		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + '0');
+			if (!(i > 9))
+			{
+				_putchar(i + '0');
+			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
