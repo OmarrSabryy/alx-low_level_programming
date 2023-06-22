d1ead4116c1135f0b7555b7fb180bc5199547a3e
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-22 09:49:59 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,8 +17,11 @@ void more_numbers(void)
 			{
 				_putchar(i + '0');
 			}
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
