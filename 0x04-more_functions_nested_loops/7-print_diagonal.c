fe33236f344f29efc62d62c6238a68b4db77b142
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-22 15:22:51 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * rint_diagonal - print a diagonal
+ * @n: integer
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int x = 0;
+
+	if (x > 0)
+	{
+		while (x < n)
+		{
+			int i = 0;
+
+			while (i < x)
+			{
+				_putchar(' ');
+				i++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			x++;
+		}
+		_putchar('\n')
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
