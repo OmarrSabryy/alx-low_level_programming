91774e63b5807d895b531b6cfff6de74c5eb7d29
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-26 23:40:38 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - prints string
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int l;
+	int i;
+
+	l = strlen(str);
+	for (i = 0; i < l; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
