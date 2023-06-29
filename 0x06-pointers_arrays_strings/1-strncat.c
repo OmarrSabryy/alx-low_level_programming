285670f3c9d998b84e84a773bcd393ef49b4bd15
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-29 08:18:13 -0700

    concat string n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,10 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int l_dest = strlen(dest);
+	int l_src = strlen(src);
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n < l_src ? n : l_src); i++)
 	{
 		*(dest + (l_dest + i)) = src[i];
 	}
