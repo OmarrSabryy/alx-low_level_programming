a3d1e93499c726549599015ec54460f58c327004
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-29 14:30:16 -0700

    copy string n bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,23 @@ char *_strncpy(char *dest, char *src, int n)
 	int l_src = strlen(src);
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= l_src)
 	{
-		*(dest + i) = s[i];
+		for (i = 0; i < n; i++)
+		{
+			*(dest + i) = src[i];
+		}
+	}
+	else
+	{
+		for (i = 0; i < l_src; i++)
+		{
+			*(dest + i) = src[i];
+		}
+		for (i = l_src; i < n; i++)
+		{
+			*(dest + i) = '\0';
+		}
 	}
 	return (dest);
 }
