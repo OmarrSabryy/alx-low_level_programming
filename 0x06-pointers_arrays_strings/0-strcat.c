5ba9fe9f342668c81417e5890359827160d6dbd6
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-29 07:42:12 -0700

    concat string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include <string.h>
+/**
+ * _strcat - concat strings
+ * @dest: pointer to string
+ * @src: pointer to string
+ *
+ * Return: pointer to string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l_dest = strlen(dest);
+	int l_src = strlen(src);
+	int i;
+
+	for (i = 0; i < l_src; i++)
+	{
+		*(dest + l_dest) = src[i];
+		l_dest++;
+	}
+	return dest;
+}
