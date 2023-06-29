a066f1b8e94216e62f9e8d17070bbb39d9d4ff5c
OmarrSabryy <omerrsabryy@gmail.com>
2023-06-29 12:39:31 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include <string.h>
+/**
+ * cap_string - capitalize string
+ * @s: pointer to string
+ *
+ * Return: pointer to string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	char str[] = {' ', ',', ';', '.', '!', '?', '(', ')', '{', '}', '\n'};
+	int l_str = strlen(str);
+	int l = strlen(s);
+
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] > 97 && s[i] < 122)
+		{
+			if (i == 0)
+			{
+				int a = s[i] - 32;
+				*(s + i) = a;
+			}
+			for (j = 0; j < l_str; j++)
+			{
+				if (s[i - 1] == s[j])
+				{
+					int a = s[i] - 32;
+					*(s + i) = a;
+				}
+			}
+		}
+	}
+	return (s);
+}
