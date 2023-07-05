d3c051569265dd2a9301e626f55a70b5233c1ef5
OmarrSabryy <omerrsabryy@gmail.com>
2023-07-05 04:11:34 -0700

    factorial recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+/**
+ * factorial - get factorial
+ * @n: integer
+ *
+ * Return: integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 int _strlen_recursion(char *s);
+int factorial(int n);
