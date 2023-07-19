001d477f2161abcf24ea024af0b806150e5c96db
OmarrSabryy <omerrsabryy@gmail.com>
2023-07-19 12:22:52 -0700

    pointers function

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - print name
  * @name: name
@@ -5,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if (name && f)
 	{
 		f(name);
 	}
