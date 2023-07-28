8cd3034e34c3e8ea68c48008f4f528c301412981
OmarrSabryy <omerrsabryy@gmail.com>
2023-07-27 18:07:57 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = NULL;
+	if (!newNode)
+	{
+		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		*head = newNode;
