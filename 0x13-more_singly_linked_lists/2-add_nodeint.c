b2722db4644606e97b7e7e2d426d92cd0f01e291
OmarrSabryy <omerrsabryy@gmail.com>
2023-07-31 11:46:05 -0700

    mll

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	newNode->n = n;
 	newNode->next = *head;
-	if (!newNOde)
+	if (!newNode)
 	{
 		return (NULL);
 	}
