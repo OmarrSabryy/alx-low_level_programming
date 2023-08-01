187c694e870d104e2e50c1cde9b1e3e33355d554
OmarrSabryy <omerrsabryy@gmail.com>
2023-08-01 09:34:11 -0700

    mll

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - remove head node
+ * @head: pointer to node address
+ *
+ * Return: element of removed node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int m;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	m = temp->n;
+	temp = temp->next;
+	return (m);
+}
